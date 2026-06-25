974dfed613b72a1403b83ab6bbc6d03426ca262e
sst19810-test <sst19810@gmail.com>
2026-06-25 20:47:43 +0530

    Update UserController.java

--- src/main/java/com/example/vulnerable/controller/UserController.java
+++ src/main/java/com/example/vulnerable/controller/UserController.java
@@ -18,8 +18,8 @@ public class UserController {
     // VULNERABILITY: SQL Injection
     @GetMapping("/search")
     public List<User> searchUsers(@RequestParam String username) {
-    return userService.searchUsersByUsername(username);
-   // return null;
+    //return userService.searchUsersByUsername(username);
+   return null;
     }
 
     // VULNERABILITY: Mass Assignment
